5602cb29e56889d9dbf853488bd0f9ff1576da41
Juan Rodas <juan.rodas@doktuz.com>
2016-05-10 18:24:12 -0500

    Aplicando WSDL al server y al client

--- wsdl_client.php
+++ wsdl_client.php
@@ -1,43 +1,12 @@
 <?php
-
-$wsclient = new WSClient( array(
-                "wsdl" => "HelloWorld.wsdl",
-                "to" => "https://wd2.doktuz.com/dok-webservice/wsdl_server,php"));
-
-$proxy = $wsclient->getProxy();
-
-$ret_val = $proxy->HelloWorld(array("$msg" => "Hola Mundo");
-
-print_r($ret_val);
-
-//$client = new SoapClient("http://local.doktuz.app/dok-webservice/wsdl_server_paciente.php?wsdl");
-//$result = $client->myFirstRequest(array("msg" => "Hola mundo"));
-//$result = $soapclient->call( 'myFirstRequest' , array("msg" => "dasdasda") );
-
-/*require_once "lib/nusoap.php";
-
-$client = new nusoap_client("http://local.doktuz.app/dok-webservice/wsdl_server_paciente.php");
+require_once "lib/nusoap.php";
+$client = new nusoap_client("https://wd2.doktuz.com/dok-webservice/wsdl_server.php");
 
 $error = $client->getError();
 if ($error) {
     echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
 }
 
-$result = $client->call("getInfo", array("profile" => "1"));
+$result = $client->call("HelloWorld", array("msg" => "hoola mundo"));
 
-if ($client->fault) {
-    echo "<h2>Fault</h2><pre>";
-    print_r($result);
-    echo "</pre>";
-}
-else {
-    $error = $client->getError();
-    if ($error) {
-        echo "<h2>Error</h2><pre>" . $error . "</pre>";
-    }
-    else {
-        echo "<h2>Books</h2><pre>";
-        echo $result;
-        echo "</pre>";
-    }
-}*/
+print_r($result);
--- wsdl_server.php
+++ wsdl_server.php
@@ -1,5 +1,5 @@
 <?php
-//require_once "lib/nusoap.php";
+require_once "lib/nusoap.php";
 
 function HelloWorld($msg) {
 
@@ -16,32 +16,6 @@ function HelloWorld($msg) {
 }
 
 
-/* Map of the service operation "ExamResult" to php function "ExamResult" */
-$operations = array("HelloWorld" => "HelloWorld");
-$opParams   = array("HelloWorld" => "MIXED");
-
-$svr = new WSService(array("wsdl"       => "HelloWorld.wsdl",
-                           "operations" => $operations,
-                           "opParams"   => $opParams));
-
-/* Reply the client */
-$svr->reply();
-
-/*$server = new soap_server();
-$server->configureWSDL("wsdl_server_paciente", "urn:wsdl_server_paciente");
-
-$namespace = "http://local.doktuz.app/dok-webservice/wsdl_server_paciente.php";
-// Register your function
-$server->register("myFirstRequest",      // Register Method
- array('msg' => 'xsd:string'),    // Method Parameter
- array('return' => 'xsd:string'), // Response
- $namespace,                      // Namespace
- false,                           // soapaction: (use default)
- "rpc",                           // style: rpc or document
- "encoded",                       // use: encoded or literal
- "Some Description"               // description: documentation for the method
-);
-
- $POST_DATA = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : '';
-
- @$server->service($POST_DATA);*/
+$server = new soap_server();
+$server->register("HelloWorld");
+$server->service($HTTP_RAW_POST_DATA);
